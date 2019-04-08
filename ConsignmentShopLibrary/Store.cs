c2229948fdf5807972daaed9a3590b04b1a74beb
Jared Jeseo <jjeseo@icloud.com>
2019-04-08 16:33:43 -0400

    First Commit

--- ConsignmentShopLibrary/Store.cs
+++ ConsignmentShopLibrary/Store.cs
@@ -12,5 +12,10 @@ namespace ConsignmentShopLibrary
         public List<Vendor> Vendors { get; set; }
         public List<Item> Items { get; set; }
 
+        public Store()
+        {
+            Vendors = new List<Vendor>();
+            Items = new List<Item>();
+        }
     }
 }
--- ConsignmentShopUI/ConsignmentShop.cs
+++ ConsignmentShopUI/ConsignmentShop.cs
@@ -11,18 +11,29 @@ using System.Windows.Forms;
 
 namespace ConsignmentShopUI
 {
-    
+
     public partial class ConsignmentShop : Form
     {
         private Store store = new Store();
+
         public ConsignmentShop()
         {
             InitializeComponent();
+            SetupData();
         }
 
         private void SetupData()
         {
+            store.Vendors.Add(new Vendor { FirstName = "Bill", LastName = "Smith" });
+            store.Vendors.Add(new Vendor { FirstName = "Sue", LastName = "Jones" });
 
+            store.Items.Add(new Item
+            {
+                Title = "Moby Dick",
+                Description = "A book about a whale.",
+                Price = 4.50M,
+                Owner = store.Vendors[0]
+            });
         }
     }
 }
